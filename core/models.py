f1757d1a3a3995245d137ec626ff66f50e7b5d4f
sumitmaurya151 <66131841+sumitmaurya151@users.noreply.github.com>
2021-06-25 17:49:05 +0530

    some help filed update of model

--- core/models.py
+++ core/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class recruiter(models.Model):
-    recruiter_name=models.CharField(max_length=100)
-    recruiter_pic=models.ImageField(upload_to='recruiters/',max_length=100)
+    recruiter_name=models.CharField(max_length=100,help_text='recruiter name can be up to 100 characters')
+    recruiter_pic=models.ImageField(upload_to='recruiters/',max_length=100 ,help_text='Please upload image in following types only GIF, JPG (or JPEG), PNG and BMP.')
 
 class announcement(models.Model):
-    date=models.DateField()
+    date=models.DateField(help_text='Enter announcement date which is visible on News and announcement section')
     title=models.CharField(max_length=70)
     description=models.CharField(max_length=500)
