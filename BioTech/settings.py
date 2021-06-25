ec79bb1dc805d691c164ee55ab0805eafdcef943
sumitmaurya151 <66131841+sumitmaurya151@users.noreply.github.com>
2021-06-25 20:06:58 +0530

    host update

--- BioTech/settings.py
+++ BioTech/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '18f#ay6@%pqm9^szw7bli!@68^2u58*#y6kisq+31038+s-hd$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'bietdbt.herokuapp.com'
+]
 
 
 # Application definition
