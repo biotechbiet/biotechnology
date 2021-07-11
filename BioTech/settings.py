db51aab9fbbcc6c21b116ed2263e609dfa061c25
sumitmaurya151 <66131841+sumitmaurya151@users.noreply.github.com>
2021-07-11 16:19:47 +0530

    bugs

--- BioTech/settings.py
+++ BioTech/settings.py
@@ -130,7 +130,33 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 if not DEBUG:
-    from .aws.conf import *
+    import datetime
+    AWS_ACCESS_KEY_ID = "AKIA4YD3LKBZRTM3DVDJ"
+    AWS_SECRET_ACCESS_KEY = "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"
+    AWS_FILE_EXPIRE = 200
+    AWS_PRELOAD_METADATA = True
+    AWS_QUERYSTRING_AUTH = True
+    AWS_DEFAULT_ACL=None
+    DEFAULT_FILE_STORAGE = 'BioTech.aws.utils.MediaRootS3BotoStorage'
+    STATICFILES_STORAGE = 'BioTech.aws.utils.StaticRootS3BotoStorage'
+    AWS_STORAGE_BUCKET_NAME = 'bietbiotechbucket'
+    # S3DIRECT_REGION = 'ap-south-1'
+    AWS_S3_REGION_NAME='ap-south-1'
+    AWS_S3_SIGNATURE_VERSION = "s3v4"
+    S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+    MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+    MEDIA_ROOT = MEDIA_URL
+    STATIC_URL = S3_URL + 'static/'
+    ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+
+    # two_months = datetime.timedelta(days=61)
+    # date_two_months_later = datetime.date.today() + two_months
+    # expires = date_two_months_later.strftime("%A, %d %B %Y 20:00:00 GMT")
+
+    # AWS_HEADERS = { 
+    #     'Expires': expires,
+    #     'Cache-Control': 'max-age=%d' % (int(two_months.total_seconds()), ),
+    # }
 else:
     STATIC_URL = '/static/'
     MEDIA_URL='/media/'
