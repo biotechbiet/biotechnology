c077e64bee79d5754c9f13bd112ca75fbfb6033d
sumitmaurya151 <66131841+sumitmaurya151@users.noreply.github.com>
2021-07-11 23:06:06 +0530

    index page updates

--- BioTech/settings.py
+++ BioTech/settings.py
@@ -148,6 +148,14 @@ if True:
     MEDIA_ROOT = MEDIA_URL
     STATIC_URL = '/static/'
     STATIC_ROOT=BASE_DIR / 'static'
+    # two_months = datetime.timedelta(days=61)
+    # date_two_months_later = datetime.date.today() + two_months
+    # expires = date_two_months_later.strftime("%A, %d %B %Y 20:00:00 GMT")
+
+    # AWS_HEADERS = { 
+    #     'Expires': expires,
+    #     'Cache-Control': 'max-age=%d' % (int(two_months.total_seconds()), ),
+    # }
 # else:
 #     STATIC_URL = '/static/'
 #     MEDIA_URL='/media/'
